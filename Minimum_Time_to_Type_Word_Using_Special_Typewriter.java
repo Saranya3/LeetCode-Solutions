62cd580cff398b3a885398a5774e3641203357b6
Saranya <38091989+Saranya3@users.noreply.github.com>
2021-08-21 22:00:56 +0530

    Create Minimum_Time_to_Type_Word_Using_Special_Typewriter.java

---
+++ Minimum_Time_to_Type_Word_Using_Special_Typewriter.java
@@ -0,0 +1,9 @@
+  int cnt = 0;
+        char prev = 'a';
+        for (int i = 0; i < word.length(); ++i) {
+            char cur = word.charAt(i);
+            int diff = Math.abs(cur - prev);
+            cnt += 1 + Math.min(diff, 26 - diff);
+            prev = cur;
+        }
+        return cnt;
