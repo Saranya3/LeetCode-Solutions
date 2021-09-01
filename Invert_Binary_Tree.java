05f4e9808db523d308102057c7ccd35d86f58a59
Saranya <38091989+Saranya3@users.noreply.github.com>
2021-09-01 12:39:42 +0530

    Update Invert_Binary_Tree.java

--- Invert_Binary_Tree.java
+++ Invert_Binary_Tree.java
@@ -1,3 +1,27 @@
+//Faster
+//Swap
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        return invert(root);
+    }
+    
+    TreeNode invert(TreeNode node) {
+        if(node==null) {
+            return null;
+        }
+        
+        // TreeNode left = node.left;
+        // TreeNode right = node.right;
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        
+        invert(node.left);
+        invert(node.right);
+        return node;
+    }
+    
+}
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         
