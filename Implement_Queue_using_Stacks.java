1ca5faa1eb7436214dc3c80fd575a64b15a1126c
Saranya <38091989+Saranya3@users.noreply.github.com>
2021-09-01 20:35:16 +0530

    Update Implement_Queue_using_Stacks.java

--- Implement_Queue_using_Stacks.java
+++ Implement_Queue_using_Stacks.java
@@ -1,3 +1,6 @@
+
+//1
+//Pop based
 class MyQueue {
     
     Stack<Integer> s1;
@@ -41,6 +44,41 @@ class MyQueue {
         if(s1.isEmpty() && s2.isEmpty())
             return true;
         return false;
+    }
+}
+//2
+//Push based
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        while(!stack1.isEmpty())
+            stack2.push(stack1.pop());
+        stack1.push(x);
+        while(!stack2.isEmpty())
+            stack1.push(stack2.pop());
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack1.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return stack1.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty();
     }
 }
 
