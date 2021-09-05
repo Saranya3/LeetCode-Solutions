88dc6d17d9c9102c542892bb7e87b0623ce4ad65
Saranya <38091989+Saranya3@users.noreply.github.com>
2021-09-05 16:18:03 +0530

    Create Reverse_Bits.java

---
+++ Reverse_Bits.java
@@ -0,0 +1,15 @@
+//1
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            if((n & 1) == 1) res++;
+            n >>= 1;
+        }
+        return res;
+    }
+}
+//2
+Integer.reverse(n);
