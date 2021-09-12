2a140c70527c6774b3d36abbeaeb1d770d10a19b
Saranya <38091989+Saranya3@users.noreply.github.com>
2021-09-12 18:34:51 +0530

    Update Number_of_Provinces.java

--- Number_of_Provinces.java
+++ Number_of_Provinces.java
@@ -1,3 +1,28 @@
+/*
+547. Number of Provinces
+--------------------------
+
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is 
+connected directly with city c, then city a is connected indirectly with city c.
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly 
+connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+ 
+
+Example 1:
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+
+Example 2:
+Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+
+*/
+
+
 class Solution {
     void dfs(int[][] isConnected,int i,int j){
         if(i<0||j<0||i>=isConnected.length||j>=isConnected[0].length||isConnected[i][j]==0){
