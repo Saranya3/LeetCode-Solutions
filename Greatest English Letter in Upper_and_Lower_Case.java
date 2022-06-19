49f961dcbaa385f8b2c7dc96ea251d32a634bc72
Saranya <38091989+Saranya3@users.noreply.github.com>
2022-06-19 19:56:16 +0530

    Create Greatest English Letter in Upper_and_Lower_Case.java

---
+++ Greatest English Letter in Upper_and_Lower_Case.java
@@ -0,0 +1,42 @@
+/*
+2309. Greatest English Letter in Upper and Lower Case
+-------------------------------------------------------
+
+Given a string of English letters s, return the greatest English letter which occurs as both a lowercase and uppercase letter in s. 
+The returned letter should be in uppercase. If no such letter exists, return an empty string.
+An English letter b is greater than another letter a if b appears after a in the English alphabet.
+
+Example 1:
+Input: s = "lEeTcOdE"
+Output: "E"
+Explanation:
+The letter 'E' is the only letter to appear in both lower and upper case.
+
+Example 2:
+Input: s = "arRAzFif"
+Output: "R"
+Explanation:
+The letter 'R' is the greatest letter to appear in both lower and upper case.
+Note that 'A' and 'F' also appear in both lower and upper case, but 'R' is greater than 'F' or 'A'.
+
+Example 3:
+Input: s = "AbCdEfGhIjK"
+Output: ""
+Explanation:
+There is no letter that appears in both lower and upper case.
+
+*/
+
+class Solution {
+    public String greatestLetter(String s) {
+        Set<Character> set=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            set.add(s.charAt(i));
+        }
+        for(char c='z';c>='a';c--){
+            if(set.contains(c) && set.contains(Character.toUpperCase(c)))
+                return Character.toUpperCase(c)+"";
+        }
+        return "";
+    }
+}
